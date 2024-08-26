6b152c58815078e4187c728d6bf0fa0db4f44b8c
Robin Peters <robin.peters@fecher.eu>
2024-08-26 11:14:27 +0200

    update

---
+++ Common/Converters/RelativeBitMapSourceConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MVVM.Base.Common.Converters
+{
+    public class RelativeBitMapSourceConverter : MarkupConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var imagePath = value.ToString() ;
+            var imageSource = new BitmapImage();
+            imageSource.BeginInit();
+            imageSource.UriSource = new Uri(imagePath, UriKind.Relative);
+            imageSource.EndInit();
+            //it needs some access
+            _ = imageSource.Height;
+            return imageSource;
+        }
+    }
+}
---
+++ Common/Converters/RelativeUriConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MVVM.Base.Common.Converters
+{
+    public class RelativeUriConverter : MarkupConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Uri)
+            {
+                return value;
+            }
+
+            return new Uri(value.ToString(), UriKind.Relative);
+        }
+    }
+}
